f351fdb91b2e5b383198e2be94c24af7c8eaaa07
isikoro <12ec018@ms.dendai.ac.jp>
2015-08-30 04:38:51 +0900

    制空値計算機
    艦これ用制空値の計算機のコンソールアプリケーションです。a√bの和をひたすら出すだけです。マイナス値を入力すると終了しようとしますが上手くいきません。

---
+++ src/kancolle/Seikuuti.java
@@ -0,0 +1,40 @@
+package kancolle;
+import java.util.Scanner;
+
+public class Seikuuti {
+
+	public static void main(String[] args) {
+		String yorn = "no";
+		Scanner stdIn = new Scanner(System.in);
+		int sum = 0;
+		int taikuuti = 0;
+		int tousaisuu = 0;
+		while(yorn == "n" || yorn == "no"){
+			System.out.println("制空値を計算します。");
+
+			while(true){
+				System.out.println("艦載機の対空値を入力してください。");
+				taikuuti = stdIn.nextInt();
+				if(taikuuti<0)
+					break;
+				System.out.println("艦載機の搭載数を入力してください。");
+				tousaisuu = stdIn.nextInt();
+				if(tousaisuu<0)
+					break;
+				sum += taikuuti * Math.sqrt(tousaisuu);
+				System.out.println("現在の制空値は"+sum+"です。(端数切捨て)");
+
+			}
+			while(true){
+				System.out.println("終了しますか？[y/n]");
+				yorn = stdIn.next();
+				System.out.print(yorn + "が入力されました。");
+				if(yorn == "y" || yorn == "n" || yorn == "yes" || yorn == "no" )
+					break;
+				System.out.print("なんですか？");
+			}
+
+		}
+
+	}
+}
